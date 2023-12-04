af745605e714452dfa279055ddf4820addf0f34e
Cyrus <Cyrus@DESKTOP-04V3736>
2023-12-04 23:16:46 +0300

    first commit

---
+++ Documents/NetBeansProjects/Assignment_1/src/assignment/Assignment.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello 123");
+        
+        
+        /*
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello" + name);
+        scan.close();
+*/
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+    }
+    
+}
